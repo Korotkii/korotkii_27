712e0c2167458ceed96db6db332d2b9bbe682f4c
Korotkii <korotki27@gmail.com>
2019-07-07 12:49:55 +0300

    Поле для игры в крестики нолики начало

---
+++ tic-tac-toe/game/src/index.js
@@ -0,0 +1,59 @@
+// import React from 'react';
+// import ReactDom from 'react-dom';
+// import './App.css';
+// import { TimerWrapper, Button, TimerDisplay } from './Clock';
+
+
+// const el=( 
+//     <div className="disp">
+
+//         <div className="title_game" >
+//             <h4 className="title_text">Tic tac toe </h4>
+//         </div>
+        
+//         <div className = "pole">
+//             <div className="player_name">
+//                 <div className="first_player">
+
+//                 </div>
+//                 <div className="second_player">
+                    
+//                 </div>
+//             </div>
+
+//             <div className="my-flex-container">
+//                 <div className="my-flex-block-A1">
+//                 </div>
+//                 <div className="my-flex-block-A2">
+//                 </div>
+//                 <div className="my-flex-block-A3">
+//                 </div>
+//                 <div className="my-flex-block-B1">
+//                 </div>
+//                 <div className="my-flex-block-B2">
+//                 </div>
+//                 <div className="my-flex-block-B3">
+//                 </div>
+//                 <div className="my-flex-block-C1">
+//                 </div>
+//                 <div className="my-flex-block-C2">
+//                 </div>
+//                 <div className="my-flex-block-C3">
+//                 </div>          
+//             </div>
+//             <div className="time">
+//                     {/* <TimerWrapper/>
+//                     <Button/>
+//                     <TimerDisplay/> */}
+//                 </div>
+                
+//                 <div className="button">
+//                     <button type="button" className="btn btn-primary">Surrender</button>
+//                 </div>           
+//         </div>
+
+//     </div>        
+    
+    
+// );
+// ReactDom.render(el, document.getElementById('root'));
