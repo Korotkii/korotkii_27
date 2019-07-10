6536e4665a2f8df55d6959f3782181aa1c887dd7
Korotkii <korotki27@gmail.com>
2019-07-10 07:12:17 +0300

    не победил функцию клика на div, он ее в упор не видит!!

--- tic-tac-toe/game/src/AddGame.js
+++ tic-tac-toe/game/src/AddGame.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import { Clock } from './Clock';
+import { Timer } from './Timer';
 import './AddGame.css';
+
 export class GamePole extends React.Component {
     
-    function game() { 
+    render() { 
+        // let games = localStorage.get("games");
+        // { field: [[1, 0, ], [0, , ], [ , , ]]}
+        // for (i) {
+        //     for(j) {
+                    // <Cell class="my-flex-block-"+i+j onClick={clickcell.bind(this, i,j)}/>
+        //     }
+        // }
         return (
-        <div className="disp">
+            <div className="disp">
 
             <div className="title_game" >
                 <h4 className="title_text">Tic tac toe </h4>
@@ -15,15 +23,19 @@ export class GamePole extends React.Component {
             <div className = "pole">
                 <div className="player_name">
                     <div className="first_player">
-
+                        <div className="name_1">
+                            <h4>"Player 1"</h4>
+                        </div>
                     </div>
                     <div className="second_player">
-                        
+                        <div className="name_2">
+                            <h4>"Player 2"</h4>
+                        </div>
                     </div>
-                </div>
+            </div>
 
                 <div className="my-flex-container">
-                    <div className="my-flex-block-A1">
+                    <div className="my-flex-block-A1"  onClick={cellclick} >
                     </div>
                     <div className="my-flex-block-A2">
                     </div>
@@ -43,7 +55,7 @@ export class GamePole extends React.Component {
                     </div>          
                 </div>
                 <div className="time">
-                    <Clock/>     
+                    <Timer/>  
                 </div>
                     
                     <div className="button">
@@ -54,5 +66,10 @@ export class GamePole extends React.Component {
         </div>        
         
     );
+          
+    }
+    cellclick(i,j) { 
+        alert ("JFDWCHJD");
+    }
+    
 }
-// export default game;
--- tic-tac-toe/game/src/Timer.js
+++ tic-tac-toe/game/src/Timer.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDom from 'react-dom';
+
 export class Timer extends React.Component {
-        constructor(props){
+    constructor(props){
         super(props);
         this.state = {
             currentTime: null
@@ -14,9 +15,10 @@ export class Timer extends React.Component {
     clockLauncher(){
         let timer = setInterval(()=>{ 
             var time = this.state.currentTime +1
-            // if (time === 5){
-            //     clearInterval(timer)
-            // }
+            // var minut = this.state.currentTime / 60
+            if (time === 300){
+                clearInterval(timer)
+            }
             this.setState({
                 currentTime: time 
             })
@@ -24,7 +26,7 @@ export class Timer extends React.Component {
     }
     
     render(){
-        return <div className ="timer">Времени прошло:{this.state.currentTime}</div>
+        return <div className ="timer">Времени прошло: {this.state.currentTime}</div>
     }
 }
 
@@ -99,68 +101,3 @@ export class Timer extends React.Component {
 
 
 
-// class TimerWrapper extends React.Component {
-//     constructor (props){
-//         super(props)
-//         this.startTimer = this.startTimer.bind(this)
-//         this.state = {
-//             timeLeft: null,
-//             timer: null
-//         }
-//     }
-//     startTimer(timeLeft){
-//         clearInterval(this.state.timer)
-//         let timer = setInterval(()=>{
-//             var timeLeft = this.state.timeLeft - 1
-//             if (timeLeft === 0){
-//                 clearInterval(timer)
-//             }
-//             this.setState({
-//                 timeLeft: timeLeft
-//             })
-//         }, 1000)
-//         return this.setState({timeLeft: timeLeft, timer: timer})
-//     }
-   
-//     render(){
-//         return(
-//             <div>
-//                 <H2>Timer</H2>
-//                 <div>
-//                     <Button time="5"  startTimer={this.startTimer}/>
-//                     <Button time="10" startTimer={this.startTimer}/>
-//                     <Button time="15" startTimer={this.startTimer}/>
-//                 </div>
-//                 <TimerDisplay timeLeft={ this.state.timeLeft}/>
-//                 <audio id="end" preload="auto" src="media/timer-bell_m1tycbno.mp3"></audio>
-//             </div>
-//         )
-//     }
-
-// }
-// class Button extends React.Component {
-//     handlerstartTimer(){
-//         return this.props.startTimer(this.props.time)
-//     }
-//     render () {
-//         return <button onClick={this.handlerstartTimer.bind(this)}>
-//         {this.props.time} секунд </button>
-//     }
-    
-// }
-// class TimerDisplay extends React.Component {
-//     render() {
-//         if(this.props.timeLeft === 0){
-//         document.getElementById("end").play()
-//         }
-//         if(this.props.timeLeft=== 0 || this.props.timeLeft == null){
-//             return <div></div>
-//         }
-//         return <H1> Времени осталось: {this.props.timeLeft}</H1>
-//     }
-// }
-
-// ReactDOM.render(
-// <TimerWrapper/>,
-// document.getElementById('content')
-// );
